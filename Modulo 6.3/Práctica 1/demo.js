d243992d5cc530f82970dea74ae00f32eb8f1774
felisuco069 <70918916+felisuco069@users.noreply.github.com>
2021-05-19 10:54:34 +0200

    Primera práctica del módulo 6.3

---
+++ Modulo 6.3/Práctica 1/demo.js
@@ -0,0 +1,60 @@
+var WORK_HOURS = [
+    "08:00 - 09:00",
+    "09:00 - 10:00",
+    "10:00 - 11:00",
+    "11:00 - 12:00",
+    "12:00 - 13:00",
+    "13:00 - 14:00",
+    "15:00 - 16:00",
+    "16:00 - 17:00"
+];
+
+var myTeam = [{
+        name: "María",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Pedro",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Esther",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Marcos",
+        availability: new Array(8).fill(true)
+    },
+];
+
+var arrayAvailability = [];
+var getRandom = (a, b) => (Math.random() < 0.5 ? a : b);
+
+for (var member of myTeam) {
+    console.log("------------------------")
+    console.log("Disponibilidad de " + member.name);
+    for (i = 0; i < member.availability.length; i++) {
+        member.availability[i] = WORK_HOURS[i] + ":  " + getRandom("Si", "No");
+        console.log(member.availability[i]);
+        if (member.availability[i] === WORK_HOURS[i] + ":  " + "Si") {
+            arrayAvailability.push(i);
+        }
+    }
+}
+
+// console.log("Aquí están los horarios disponibles de todos: " + arrayAvailability); Esto lo he usado para ir comprobando
+var myArray = new Array(8).fill(0)
+var hour = null;
+for (s = 0; s < arrayAvailability.length; s++) {
+    myArray[arrayAvailability[s]] = myArray[arrayAvailability[s]] + 1;
+    if (myArray[arrayAvailability[s]] === 4) {
+        var hour = WORK_HOURS[arrayAvailability[s]];
+        break
+    }
+}
+// console.log(myArray);
+if (hour != null) {
+    console.log("La primera hora disponible para realizar una reunión es: " + hour);
+} else {
+    console.log("No hay disponibilidad para la reunión");
+}
