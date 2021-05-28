29946ece99fd50e656389e0c66d9e0c11edf82f7
felisuco069 <70918916+felisuco069@users.noreply.github.com>
2021-05-28 12:57:12 +0200

    Rectificación de ejercicio opcional, sustituyo el pattern3 por pattern5 y los renombro todos a pattern3

--- Modulo 7/demo2.js
+++ Modulo 7/demo2.js
@@ -3,9 +3,9 @@ console.log("---- Ejercicios opcionales ----");
 console.log("---- Extraer imágenes de un fichero HTML ----");
 console.log("---- Caso 1 ----");
 
-const pattern3 = /^<img\ssrc="(.+)"\/>$/
+const pattern3 = /^\s*<img\s*src="(.+)"\s*\/>$/;
 const tag = '<img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>'
-console.log("De la etiqueta " + tag + " dada, la parte dentro de src es: ")
+console.log("De la etiqueta " + tag + " dada, la url es: ")
 console.log(tag.match(pattern3)[1]);
 
 console.log("---- Caso 2 ----");
@@ -24,17 +24,16 @@ const textHtml = `<html>
 </html>`;
 const textTag = textHtml.match(pattern4);
 console.log(textTag);
-const pattern5 = /^\s*<img\s*src="(.+)"\s*\/>$/;
 textTag.forEach((element, index) =>
-    console.log("La dirección de la foto " + (index+1) + " es: " + element.match(pattern5)[1])
+    console.log("La dirección de la foto " + (index+1) + " es: " + element.match(pattern3)[1])
 );
 
-console.log("---- Validar sintaxis tarjeta de crédito ----");
-console.log("---- Caso 1 ----");
-const pattern6 = /^(5[0-5]\d{2})(\s|-)?(\d{4})(\s|-)?(\d{4})(\s|-)?(\d{4})$/;
-const numberCard = ["5299 6400 0000 0000", "5299-6400-0000-0000" , "5299640000000000"];
-console.log("---- Caso 2 ----");
-numberCard.forEach(card => console.log("Para la tarjeta " + card + " los grupos de números son: Grupo 1 " + card.match(pattern6)[1]+ " Grupo 2 " + card.match(pattern6)[3] + " Grupo 3 " + card.match(pattern6)[5] + " Grupo 4 " + card.match(pattern6)[7]));
+// console.log("---- Validar sintaxis tarjeta de crédito ----");
+// console.log("---- Caso 1 ----");
+// const pattern5 = /^(5[0-5]\d{2})(\s|-)?(\d{4})(\s|-)?(\d{4})(\s|-)?(\d{4})$/;
+// const numberCard = ["5299 6400 0000 0000", "5299-6400-0000-0000" , "5299640000000000"];
+// console.log("---- Caso 2 ----");
+// numberCard.forEach(card => console.log("Para la tarjeta " + card + " los grupos de números son: Grupo 1 " + card.match(pattern5)[1]+ " Grupo 2 " + card.match(pattern5)[3] + " Grupo 3 " + card.match(pattern5)[5] + " Grupo 4 " + card.match(pattern5)[7]));
 
 
 
